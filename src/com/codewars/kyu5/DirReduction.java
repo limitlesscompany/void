1b1972e02152c2993f47c17d92093749badc5e8e
cyberloli <cyberloli@matrix>
2020-01-25 19:13:03 +0300

    DirReduction kata done

--- src/com/codewars/kyu5/DirReduction.java
+++ src/com/codewars/kyu5/DirReduction.java
@@ -1,4 +1,68 @@
 package com.codewars.kyu5;
 
+import java.util.*;
+
 public class DirReduction {
+
+    enum Directions {SOUTH, EAST, WEST, NORTH}
+
+
+
+   static  List<String> newList;
+
+    static Map<Directions, Directions> map = new HashMap<>();
+
+
+    public static void main(String[] args) {
+
+
+
+        String [] testArr =  new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"};
+
+        System.out.println(Arrays.toString(dirReduc(testArr)));
+
+
+    }
+
+
+    public static String[] dirReduc(String[] arr) {
+
+        map.put(Directions.EAST, Directions.WEST); //EAST = WEST
+        map.put(Directions.SOUTH, Directions.NORTH);// SOUTH = NORTH
+        map.put(Directions.WEST, Directions.EAST); // WEST = EAST
+        map.put(Directions.NORTH, Directions.SOUTH); //NORTH = SOUTH
+        boolean finish = false;
+
+        newList = new LinkedList<>(Arrays.asList(arr));
+    int counter = newList.size();
+        while (counter > 0) {
+
+            for (int i = newList.size() - 1; i > 0; i--) {
+
+                finish = false;
+                Directions currDirection = Directions.valueOf(newList.get(i));
+                Directions nextDirection = Directions.valueOf(newList.get(i - 1));
+
+                if (nextDirection == map.get(currDirection))  {
+                        newList.remove(i);
+                        newList.remove(i - 1);
+                        i--;
+                }
+
+
+            }
+
+            counter--;
+        }
+
+       String[] newArr = new String[newList.size()];
+
+        for (int i = 0; i < newList.size(); i++ ) {
+                newArr[i] = (String) newList.get(i);
+        }
+
+
+        return newArr;
+    }
+
 }
---
+++ src/com/codewars/kyu5/DirReductionTest.java
@@ -0,0 +1,19 @@
+package com.codewars.kyu5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runners.JUnit4;
+
+public class DirReductionTest {
+    @Test
+    public void testSimpleDirReduc() {
+        assertArrayEquals("\"NORTH\", \"SOUTH\", \"SOUTH\", \"EAST\", \"WEST\", \"NORTH\", \"WEST\"",
+                new String[]{"WEST"},
+                DirReduction.dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}));
+        assertArrayEquals("\"NORTH\",\"SOUTH\",\"SOUTH\",\"EAST\",\"WEST\",\"NORTH\"",
+                new String[]{},
+                DirReduction.dirReduc(new String[]{"NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"}));
+    }
+}
